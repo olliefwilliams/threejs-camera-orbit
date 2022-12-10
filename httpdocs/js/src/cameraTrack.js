0c644ead144ceb9675ea55deea1233b4e47dfb24
Ollie <ofw@hotmail.co.uk>
2022-12-10 12:37:49 +0100

    Added XY orbit controls

--- httpdocs/js/src/cameraTrack.js
+++ httpdocs/js/src/cameraTrack.js
@@ -7,7 +7,8 @@ function cameraTrack(camera, delta) {
 	// keep track of where we initially placed camera
 	const originalPosition = camera.position;
 
-	document.addEventListener('mousemove', (e) => {
+
+	function orbit(e) {
 		// get mouse coordinates
 		let mouseX = e.clientX;
 		let mouseY = e.clientY;
@@ -16,36 +17,16 @@ function cameraTrack(camera, delta) {
 		let winX = window.innerWidth;
 		let winY = window.innerHeight;
 
-		// convert mouse coordinates 0,0 is in the center
+		// convert X mouse coordinates 0,0 is in the center
 		// to match three.js coordinates
 		mouseX -= (window.innerWidth / 2)
-		mouseY -= (window.innerHeight / 2)
+		//mouseY -= (window.innerHeight / 2)
 
-		// now divide that by the highest it can be
+		// now divide both by that by the highest it can be
 		// to convert it into a decimal percentage equivalent
-		// Range is now +-1 for X & Y
+		// Range is now +-1 for X & 0–1 for Y
 		mouseX /= (window.innerWidth / 2)
-		mouseY /= (window.innerHeight / 2)
-
-		/*
-		// moves the camera up and down
-
-		// set factor to move the camera by
-		let cameraFactor = 2;
-		camera.position.x = cameraFactor * mouseX;
-		camera.position.y = (cameraFactor * mouseY) * -1; // invert movement
-		*/
-
-
-		// Let's move the camera left to right AROUND the cube
-		// instead of just panning in a straight line
-
-		/*
-		Parametric equation of a circle is
-		x = r * cos(a)
-		y = r * sin(a)
-		Where r is the radius, and a the angle in radians
-		*/
+		mouseY /= (window.innerHeight)
 
 		// remember we just moved the camera back from the origin, so
 		// const radius = camera.position.z; 	
@@ -53,33 +34,28 @@ function cameraTrack(camera, delta) {
 		const radius = 5;
 		const angleRad = 1.2; // max angle either side of origin
 
-		//camera.position.x = (radius * Math.sin(angleRad * mouseX));
-		//camera.position.z = (radius * Math.cos(angleRad * mouseX));
-
-		// diffrent attempt:
 		// followed from https://andreasrohner.at/posts/Web%20Development/JavaScript/Simple-orbital-camera-controls-for-THREE-js/
-		// but the co-ordinates system needed to be converted to work as they do not match Three's sytem
-		// Convert the X and Y coordinates into angles in the XZ and YZ plane
-		// theta = atan2(z, x), where x and z are the coordinates of the point in the XZ plane
-		// phi = atan2(z, y), where y and z are the coordinates of the point in the YZ plane
+		// with further help from https://dynref.engr.illinois.edu/rvs.html
+		// but the co-ordinates system needed to be converted to work as they do not match Three's system
 		// Z will always be 5, as that's how far away from the origin we moved the camera
 		let z = radius;
-		// multiply the mouse value by 5 to give a reasonable range
-		let theta = Math.atan2(radius, (mouseY * 5));
-		let phi = Math.atan2(radius, (mouseX * 5));
-		phi -= 33; // no idea why this is the correct number
+		let theta = mouseY * (angleRad + (Math.PI / 2)); // +1/4 circle as 0deg is looking at the top of the cube
+		let phi = mouseX * angleRad;
 
-		console.log(`Angle theta: ${theta}, Phi: ${phi}`);
+		//console.log(`X: ${mouseX}, Y: ${mouseY}, Theta: ${theta}, Phi: ${phi}`);
 
 		camera.position.z = radius * Math.sin(theta) * Math.cos(phi);
 		camera.position.x = radius * Math.sin(theta) * Math.sin(phi);
 		camera.position.y = radius * Math.cos(theta);
-
+		//console.log(camera.position);
 
 		// make camera look at cube
 		camera.lookAt(origin);
 
-	})
+	}
+
+
+	document.addEventListener('mousemove', orbit);
 
 
 
