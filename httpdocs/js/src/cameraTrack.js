b9d8b3cf341dcb24900d1ba54da5fbff6d80f7a2
Ollie <ofw@hotmail.co.uk>
2022-12-10 12:53:52 +0100

    Tidied and made internal logic consistent between axes

--- httpdocs/js/src/cameraTrack.js
+++ httpdocs/js/src/cameraTrack.js
@@ -17,16 +17,16 @@ function cameraTrack(camera, delta) {
 		let winX = window.innerWidth;
 		let winY = window.innerHeight;
 
-		// convert X mouse coordinates 0,0 is in the center
+		// convert X and Y mouse coordinates 0,0 is in the center
 		// to match three.js coordinates
 		mouseX -= (window.innerWidth / 2)
-		//mouseY -= (window.innerHeight / 2)
+		mouseY -= (window.innerHeight / 2)
 
 		// now divide both by that by the highest it can be
 		// to convert it into a decimal percentage equivalent
-		// Range is now +-1 for X & 0–1 for Y
+		// Range is now +-1 for X & Y
 		mouseX /= (window.innerWidth / 2)
-		mouseY /= (window.innerHeight)
+		mouseY /= (window.innerHeight / 2)
 
 		// remember we just moved the camera back from the origin, so
 		// const radius = camera.position.z; 	
@@ -35,14 +35,17 @@ function cameraTrack(camera, delta) {
 		const angleRad = 1.2; // max angle either side of origin
 
 		// followed from https://andreasrohner.at/posts/Web%20Development/JavaScript/Simple-orbital-camera-controls-for-THREE-js/
-		// with further help from https://dynref.engr.illinois.edu/rvs.html
-		// but the co-ordinates system needed to be converted to work as they do not match Three's system
-		// Z will always be 5, as that's how far away from the origin we moved the camera
-		let z = radius;
-		let theta = mouseY * (angleRad + (Math.PI / 2)); // +1/4 circle as 0deg is looking at the top of the cube
-		let phi = mouseX * angleRad;
+		// with further help from 
+		// but the co-ordinates system needed to be converted to match Three's system
 
-		//console.log(`X: ${mouseX}, Y: ${mouseY}, Theta: ${theta}, Phi: ${phi}`);
+		// Rotation on XZ plane, happy with negative numbers
+		let phi = mouseX * angleRad;
+		// Rotation on YZ plane, not happy with negative numbers
+		// 1/4 circle added at the end to bring camera down from 0deg,
+		// which would be pointing down at top of subject, see https://dynref.engr.illinois.edu/rvs.html
+		let theta = (mouseY * (angleRad)) + (Math.PI / 2);
+
+		console.log(`X: ${mouseX}, Y: ${mouseY}, Theta: ${theta}, Phi: ${phi}`);
 
 		camera.position.z = radius * Math.sin(theta) * Math.cos(phi);
 		camera.position.x = radius * Math.sin(theta) * Math.sin(phi);
